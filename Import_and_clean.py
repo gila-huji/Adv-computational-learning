4a3d0e1a5b03e61428ff43647d0851f7ceb2c566
gilao <gila.ovadia@mail.huji.ac.il>
2024-09-05 11:08:30 +0300

    Add code for model

--- Import_and_clean.py
+++ Import_and_clean.py
@@ -14,17 +14,7 @@ pd.set_option('display.max_columns', 1000)
 
 #endregion
 
-#region import
 
-## Import genomic data
-train1 = pd.read_csv(rf"C:\Users\gilao\Desktop\גנטי\Data\Genomic\train_test\set1\modkit\HG001_train", sep='\t')
-
-# import CpG data
-cpg = pd.read_csv(rf"C:\Users\gilao\Desktop\גנטי\Data\Genomic\Refrence Data\CpG_HG38\CpG_chr21.csv")
-cpg.rename(columns={'V1': 'chr', 'V2': 'ref_pos', 'V3': 'CpG_site'}, inplace=True)
-#endregion
-
-# region clean
 def clean(df):
     # get the max value of methylation between m and h
     df = df.groupby(['read_id', 'forward_read_position', 'ref_position', 'chrom', 'mod_strand', 'ref_strand', 'ref_mod_strand',
@@ -37,6 +27,27 @@ def clean(df):
 
     return(df)
 
-train1 = clean(train1)
+if __name__ == '__main__':
 
-# endregion
+    # region import
+    ## Import genomic data
+    print("Importing Data")
+    train1 = pd.read_csv(rf"C:\Users\gilao\Desktop\גנטי\Data\Genomic\train_test\set1\modkit\HG001_train", sep='\t')
+    train2 = pd.read_csv(rf"C:\Users\gilao\Desktop\גנטי\Data\Genomic\train_test\set1\modkit\HG002_train", sep='\t')
+    test1 = pd.read_csv(rf"C:\Users\gilao\Desktop\גנטי\Data\Genomic\train_test\set1\modkit\HG001_test", sep='\t')
+    test2 = pd.read_csv(rf"C:\Users\gilao\Desktop\גנטי\Data\Genomic\train_test\set1\modkit\HG002_test", sep='\t')
+    print("Data Imported")
+
+    # import CpG data
+    cpg = pd.read_csv(rf"C:\Users\gilao\Desktop\גנטי\Data\Genomic\Refrence Data\CpG_HG38\CpG_chr21.csv")
+    cpg.rename(columns={'V1': 'chr', 'V2': 'ref_pos', 'V3': 'CpG_site'}, inplace=True)
+    # endregion
+
+    # region clean
+    print("Cleaning Data")
+    train1 = clean(train1)
+    train2 = clean(train2)
+    test1 = clean(test1)
+    test2 = clean(test2)
+    print("Data Clea")
+    #endregion
